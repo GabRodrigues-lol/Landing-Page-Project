b41889d20c629447b6f238eccb424a85aa5b16e8
Gabriel Rodrigues <gabriel.rs266@gmail.com>
2025-05-07 20:54:42 -0300

    formulario

--- main.js
+++ main.js
@@ -4,13 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Página carregada.");
     document.addEventListener("DOMContentLoaded", function () {
       const form = document.getElementById("contactForm");
-    
-      form.addEventListener("submit", function (e) {
-        e.preventDefault(); // Impede o recarregamento da página
-    
-        // Exibe um pop-up com a mensagem de sucesso
-        alert("Cadastro feito com sucesso");
-      });
     });
     
   });
@@ -20,12 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const closePopup = document.getElementById("closePopup");
   
     form.addEventListener("submit", function (e) {
-      e.preventDefault(); // impede o envio padrão
-      popup.style.display = "flex"; // mostra o pop-up
+      e.preventDefault();
+      popup.style.display = "flex"; 
     });
   
     closePopup.addEventListener("click", function () {
-      popup.style.display = "none"; // esconde o pop-up
+      popup.style.display = "none";
     });
   });
   
