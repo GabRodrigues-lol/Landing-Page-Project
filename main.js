aa43aceb5c78e6abee555ba21070b5466fff68a7
Gabriel Rodrigues <gabriel.rs266@gmail.com>
2025-05-07 18:30:16 -0300

    adição da mensagem de sucesso do cadastro

--- main.js
+++ main.js
@@ -4,4 +4,22 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Página carregada.");
 
   });
+
+$(document).ready(function(){
+  $('form').on('submit', function(event){
+      event.preventDefault();
+      const $mensagemSucesso = $(
+'#mensagem-sucesso');
+
+        setTimeout(function(){
+          $mensagemSucesso.removeClass(
+'d-none');
+            setTimeout(function(){
+              $mensagemSucesso.addClass(
+'d-none');
+
+            }, 3000);
+        }, 1500);
+  });
+});
   
