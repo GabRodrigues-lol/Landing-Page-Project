3e5407b9a979e112dba55580efb7bce3c1a506d8
Gabriel Rodrigues <gabriel.rs266@gmail.com>
2025-05-07 19:56:13 -0300

    Atualização do botão do form

--- main.js
+++ main.js
@@ -2,24 +2,31 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Página carregada.");
-
+    document.addEventListener("DOMContentLoaded", function () {
+      const form = document.getElementById("contactForm");
+    
+      form.addEventListener("submit", function (e) {
+        e.preventDefault(); // Impede o recarregamento da página
+    
+        // Exibe um pop-up com a mensagem de sucesso
+        alert("Cadastro feito com sucesso");
+      });
+    });
+    
   });
-
-$(document).ready(function(){
-  $('form').on('submit', function(event){
-      event.preventDefault();
-      const $mensagemSucesso = $(
-'#mensagem-sucesso');
-
-        setTimeout(function(){
-          $mensagemSucesso.removeClass(
-'d-none');
-            setTimeout(function(){
-              $mensagemSucesso.addClass(
-'d-none');
-
-            }, 3000);
-        }, 1500);
-  });
-});
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contactForm");
+    const popup = document.getElementById("popup");
+    const closePopup = document.getElementById("closePopup");
   
+    form.addEventListener("submit", function (e) {
+      e.preventDefault(); // impede o envio padrão
+      popup.style.display = "flex"; // mostra o pop-up
+    });
+  
+    closePopup.addEventListener("click", function () {
+      popup.style.display = "none"; // esconde o pop-up
+    });
+  });
+  
+
